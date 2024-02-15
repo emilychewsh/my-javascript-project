f9000240a57e46161becf110470b5d0cde042786
Emily Chew <emilychewsh@gmail.com>
2024-02-16 01:04:18 +1100

    Fix render page on left when btn clicked, add conditionals statements

--- index.js
+++ index.js
@@ -1,7 +1,6 @@
 document.addEventListener("DOMContentLoaded", initialise);
 
 const digimonList = document.querySelector("#digimon-list")
-
 const digimonInfoLeftContainer = document.querySelector("#left-digimon-info")
 
 // Initialise async function
@@ -26,39 +25,55 @@ async function initialise() {
     }
     FilteredDigimonData()
 
+    //Fx to get rid of Digimon data on left after clicking Search, Refresh and Sort buttons
+    function resetContainer () {
+      const imageContainer = document.querySelector("#image-container")
+      imageContainer.innerHTML= ""
+      const digimonNameContainer = document.querySelector("#digimonName")
+      digimonNameContainer.innerHTML = ""
+      const digimonTypeBox = document.querySelector("#type-box-type")
+      digimonTypeBox.innerHTML = ""
+      const digimonAttributeBox = document.querySelector("#type-box-attribute")
+      digimonAttributeBox.innerHTML = ""
+      const digimonDescBox = document.querySelector("#digimon-description")
+      digimonDescBox.innerHTML = ""
+      const digimonYearBox = document.querySelector("#yearReleased")
+      digimonYearBox.innerHTML = ""
+      digimonInfoLeftContainer.classList.add("none")
+    }
 
-    // Search digimon in search bar
+    //Declare variables for button functions
     const searchInput = document.querySelector("#input")
     const searchBtn = document.querySelector("#searchBtn")
     const sortBtn = document.querySelector("#sortBtn")
     const sortBtnOriginalValue = document.querySelector("#sortBtn").innerHTML
 
+    // Search digimon in search bar
     searchBtn.addEventListener("click", () => {
+    resetContainer()
     filterDigimons = digimons.filter((digimon) => {
       return digimon.name.toLowerCase().includes(searchInput.value.toLowerCase())
     })
-      // digimonInfoLeftContainer.innerHTML = "";
-
-      //Return sort button value to original 
-      sortBtn.innerHTML = sortBtnOriginalValue
-
-      if (filterDigimons.length === 0) {
-        digimonList.innerHTML = `No Digimon with the name "${searchInput.value}" found. Please try another Digimon!`
-      } else {
-        digimonList.innerHTML = ""
-        FilteredDigimonData()
-      }
+    //Return sort button value to original 
+    sortBtn.innerHTML = sortBtnOriginalValue
+    //If no digimon with the name is found 
+    if (filterDigimons.length === 0) {
+        digimonList.innerHTML = `No Digimon with the name "${searchInput.value}" found. Please try another Digimon or click the "Refresh" button!`
+    } else {
+      digimonList.innerHTML = ""
+      FilteredDigimonData()
+    }
     })
 
     // Refresh Button to reset scroll bar on the right
     const refreshBtn = document.querySelector("#refreshBtn")
     refreshBtn.addEventListener("click", () => {
+      resetContainer()
       filterDigimons = [...digimons]
 
       //Return sort button value to original
       sortBtn.innerHTML = sortBtnOriginalValue
-      
-      // digimonInfoLeftContainer.innerHTML = "";
+
       digimonList.innerHTML = ""
       FilteredDigimonData()
     })
@@ -66,6 +81,7 @@ async function initialise() {
     //Sort Digimon in Ascending order
     const sortAscendingBtn = document.querySelector("#sortAscendingBtn")
     sortAscendingBtn.addEventListener("click", () => {
+        resetContainer()
         // console.log(filterDigimons.sort(digimon => digimon.name))
         filterDigimons.sort((a, b) => {
           return a.name.localeCompare(b.name)
@@ -77,6 +93,7 @@ async function initialise() {
     //Sort Digimon in Descending Order
     const sortDescendingBtn = document.querySelector("#sortDescendingBtn")
     sortDescendingBtn.addEventListener("click", () => {
+      resetContainer()
       filterDigimons.sort((a, b) => {
         return b.name.localeCompare(a.name)
       })
@@ -85,6 +102,7 @@ async function initialise() {
     })
 
 }
+
 
 //Fetching 100 Digimon Data from external API 
 async function fetch100Digimons(){
@@ -105,13 +123,15 @@ function getDigimon(event){
 
   .then(response => response.json())
   .then((data) => {
-    // console.log(data)
+    
     const imageElement = document.createElement("img")
     imageElement.src = data.images[0].href
+    imageElement.alt = "Failed to get image"
     const imageContainer = document.querySelector("#image-container")
     imageContainer.innerHTML = ""
     imageContainer.appendChild(imageElement)
-
+  
+  
     renderDigimonTypes(data)
     renderDigimonAttributes(data)
     renderDigimonDesc(data)
@@ -133,19 +153,29 @@ function renderDigimonName (object) {
 
 //Fx for rendering Types
 function renderDigimonTypes(object){
-  const digimonType = object.types[0].type
-  const digimonTypeBox = document.querySelector("#type-box-type")
-  digimonTypeBox.innerHTML = "Type: " + ""
-  digimonTypeBox.append(digimonType)
+  if (object.types.length > 0 ) {
+    const digimonType = object.types[0].type
+    const digimonTypeBox = document.querySelector("#type-box-type")
+    digimonTypeBox.innerHTML = "Type: " + ""
+    digimonTypeBox.append(digimonType)
+  } else {
+    const digimonTypeBox = document.querySelector("#type-box-type")
+    digimonTypeBox.innerHTML = "No type found"
+  }
+
 }
 
 //Fx for rendering Attributes
 function renderDigimonAttributes(object){
-  const digimonAttributes = object.attributes[0].attribute
-  // console.log(digimonAttributes)
-  const digimonAttributeBox = document.querySelector("#type-box-attribute")
-  digimonAttributeBox.innerHTML = "Attribute: " + ""
-  digimonAttributeBox.append(digimonAttributes)
+  if (object.attributes.length > 0){
+    const digimonAttributes = object.attributes[0].attribute
+    const digimonAttributeBox = document.querySelector("#type-box-attribute")
+    digimonAttributeBox.innerHTML = "Attribute: " + ""
+    digimonAttributeBox.append(digimonAttributes)
+  } else {
+    const digimonAttributeBox = document.querySelector("#type-box-attribute")
+    digimonAttributeBox.innerHTML = "No Attribute found"
+  }
 }
 
 //Fx for rendering Description and find method to look for only english description
@@ -154,21 +184,26 @@ function renderDigimonDesc(object){
     return desc.language === "en_us"
   })
 
-  const digimonDescBox = document.querySelector("#digimon-description")
-  const digimonParagraph = document.createElement("p")
-  digimonParagraph.textContent = englishDesc.description
-  digimonDescBox.innerHTML = ""
-  digimonDescBox.append(digimonParagraph)
+  if (englishDesc != undefined){
+    const digimonDescBox = document.querySelector("#digimon-description")
+    const digimonParagraph = document.createElement("p")
+    digimonParagraph.textContent = englishDesc.description
+    digimonDescBox.innerHTML = ""
+    digimonDescBox.append(digimonParagraph)
+  } else {
+    const digimonDescBox = document.querySelector("#digimon-description")
+    const digimonParagraph = document.createElement("p")
+    digimonParagraph.textContent = "No Description Found"
+    digimonDescBox.innerHTML = ""
+    digimonDescBox.append(digimonParagraph)
+  }
 }
 
 //Fx for rendering Digimon Year
 function renderDigimonYear(object){
-  const digimonYearBox = document.querySelector("#yearReleased")
-  const digimonYear = object.releaseDate
-  digimonYearBox.innerHTML = "Year Released: " + "" 
-  digimonYearBox.append(digimonYear)
+  
+    const digimonYearBox = document.querySelector("#yearReleased")
+    const digimonYear = object.releaseDate
+    digimonYearBox.innerHTML = "Year Released: " + "" 
+    digimonYearBox.append(digimonYear)
 }
-
-
-
-// fix within refresh and sort - to clear left digimon data
